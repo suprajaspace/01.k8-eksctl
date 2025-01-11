6169da1cd11c84fa50965107a2e36f7ac8fda282
Supraja <supra556699@gmail.com>
2025-01-11 15:46:20 +0800

    terraform ec2 module-workstation

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0a39710fe439d3d9a"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-08804b4ea8944c8da"
+}
